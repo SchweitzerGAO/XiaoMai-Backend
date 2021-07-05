bd0030879af83536caf355364bf4211984c5444f
Shankfizz <1250103947@qq.com>
2021-07-05 18:43:53 +0800

    no message

---
+++ APIs/APIs/Models/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class User
+{
+    public string ID { set; get; }
+    public string User_Name { set; get; }
+    public string Password { set; get; }
+    
+    public string Address { set; get; }
+
+}
