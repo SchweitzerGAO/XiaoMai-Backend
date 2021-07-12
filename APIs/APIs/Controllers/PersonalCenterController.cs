28ce06d920854f632269d7ec31092ba06e303ddb
Shankfizz <1250103947@qq.com>
2021-07-12 10:28:59 +0800

    添加返回参数

--- APIs/APIs/Controllers/PersonalCenterController.cs
+++ APIs/APIs/Controllers/PersonalCenterController.cs
@@ -29,7 +29,7 @@ namespace APIs.Controllers
             if (info.UserType == "CUSTOMER")
             {
                 UserInfo res = new UserInfo();
-                string Sql = @"SELECT ID,USER_NAME,ADDRESS,DATE_OF_REG,PHONE_NUMBER
+                string Sql = @"SELECT ID,USER_NAME,ADDRESS,DATE_OF_REG,PHONE_NUMBER,PHOTO
                                 FROM CUSTOMER
                                 WHERE ID ="+info.ID;
                 DataTable table = dBHelper.ExecuteTable(Sql);
@@ -47,7 +47,7 @@ namespace APIs.Controllers
             else if (info.UserType == "SELLER")
             {
                 SellerInfo res = new SellerInfo();
-                string Sql = @"SELECT ID,USER_NAME,ADDRESS,DATE_OF_REG,PHONE_NUMBER
+                string Sql = @"SELECT ID,SELLER_NAME,ADDRESS,DATE_OF_REG,PHONE_NUMBER,EARNING,PHOTO
                                 FROM SELLER
                                 WHERE ID =" + info.ID;
                 DataTable table = dBHelper.ExecuteTable(Sql);
@@ -56,8 +56,9 @@ namespace APIs.Controllers
                 res.SellerName = row["SELLER_NAME"].ToString();
                 res.Address = row["ADDRESS"].ToString();
                 res.RegDate = row["DATE_OF_REG"].ToString();
-                res.Income = int.Parse(row["EARNING"].ToString());
+                res.Income = row["EARNING"].ToString();
                 res.Image = dBHelper.GetSellerBlob(info.ID);
+                res.PhoneNumber = row["PHONE_NUMBER"].ToString();
                 
                 //前端或许需要转码，将图片从Base64转成Image
                 return Ok(dBHelper.ToJson(res));
